b79ad3640b6268dc3b459c35c1fac105137f3050
Léo Haf <leo@haf.ovh>
2025-05-21 12:28:16 +0200

    p2p: enable -rejecttokens by default

--- src/policy/policy.h
+++ src/policy/policy.h
@@ -53,7 +53,7 @@ static constexpr unsigned int DEFAULT_BYTES_PER_SIGOP_STRICT{20};
 /** Default for -datacarriercost (multiplied by WITNESS_SCALE_FACTOR) */
 static constexpr unsigned int DEFAULT_WEIGHT_PER_DATA_BYTE{4};
 /** Default for -rejecttokens */
-static constexpr bool DEFAULT_REJECT_TOKENS{false};
+static constexpr bool DEFAULT_REJECT_TOKENS{true};
 /** Default for -permitbarepubkey */
 static constexpr bool DEFAULT_PERMIT_BAREPUBKEY{false};
 /** Default for -permitbaremultisig */
