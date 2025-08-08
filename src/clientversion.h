bitcoincore-dev/bitcoin-2001-863168-294116-d812cf1189

Bitcoin Knots branding

src/clientversion.h
@@ -63,7 +63,7 @@ extern const std::string CLIENT_DATE;
 
 
 std::string FormatFullVersion();
-std::string FormatSubVersion(const std::string& name, int nClientVersion, const std::vector<std::string>& comments);
+std::string FormatSubVersion(const std::string& name, int nClientVersion, const std::vector<std::string>& comments, bool fBaseNameOnly = false);
 
 #endif // WINDRES_PREPROC
 

bitcoinknots/bitcoin

src/clientversion.h
@@ -37,7 +37,7 @@ extern const std::string UA_NAME;
 
 
 std::string FormatFullVersion();
-std::string FormatSubVersion(const std::string& name, int nClientVersion, const std::vector<std::string>& comments);
+std::string FormatSubVersion(const std::string& name, int nClientVersion, const std::vector<std::string>& comments, bool base_name_only = false);
 
 std::string CopyrightHolders(const std::string& strPrefix);
 