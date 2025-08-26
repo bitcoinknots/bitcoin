732ba0e1318ef46421b74d57a5f4e6eaf2ba9ab4
Pol Espinasa <pol.espinasa@uab.cat>
2025-08-27 00:56:17 +0200

    rpc, logging: add backgroundvalidation to getblockchaininfo
    Github-Pull: #33259
    Rebased-From: c1f545248ea0966a3aad0d70fc4c317cd9611d3f

--- test/functional/rpc_blockchain.py
+++ test/functional/rpc_blockchain.py
@@ -124,6 +124,7 @@ class BlockchainTest(BitcoinTestFramework):
         self.log.info("Test getblockchaininfo")
 
         keys = [
+            'backgroundvalidation',
             'bestblockhash',
             'blocks',
             'chain',
@@ -155,6 +156,10 @@ class BlockchainTest(BitcoinTestFramework):
         # check other pruning fields given that prune=1
         assert res['pruned']
         assert not res['automatic_pruning']
+
+        # check background validation
+        assert not res['backgroundvalidation']
+        assert 'backgroundvalidationprogress' not in res.keys()
 
         self.restart_node(0, ['-stopatheight=207'])
         res = self.nodes[0].getblockchaininfo()
