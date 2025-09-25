ce1c7f721303f98672d67e0ad811b6fdbb053a95
Marcel Stampfer <marcel.stampfer@quantalconsulting.com>
2025-09-25 09:56:50 +0100

    build: Integrate zsh completion installation into CMake
    - Add INSTALL_ZSH_COMPLETION option to CMakeLists.txt (defaults to ON)
    - Extend InstallBinaryComponent.cmake to support HAS_ZSH_COMPLETION parameter
    - Add custom CMake target to generate bitcoin-cli.zsh using tool_cli_completion.py
    - Install zsh completion to ${CMAKE_INSTALL_DATADIR}/zsh/site-functions/ with underscore prefix
    - Fix Python executable variable (use Python3_EXECUTABLE instead of Python_EXECUTABLE)
    - Works cross-platform on both macOS and Linux
    
    The zsh completion file is automatically generated during build when
    INSTALL_ZSH_COMPLETION=ON and installed to the appropriate system or
    user directory based on CMAKE_INSTALL_PREFIX.

--- cmake/module/InstallBinaryComponent.cmake
+++ cmake/module/InstallBinaryComponent.cmake
@@ -7,10 +7,10 @@ include(GNUInstallDirs)
 
 function(install_binary_component component)
   cmake_parse_arguments(PARSE_ARGV 1
-    IC                # prefix
-    "HAS_MANPAGE"     # options
-    ""                # one_value_keywords
-    ""                # multi_value_keywords
+    IC                        # prefix
+    "HAS_MANPAGE;HAS_ZSH_COMPLETION"  # options
+    ""                        # one_value_keywords
+    ""                        # multi_value_keywords
   )
   set(target_name ${component})
   install(TARGETS ${target_name}
@@ -20,6 +20,14 @@ function(install_binary_component component)
   if(INSTALL_MAN AND IC_HAS_MANPAGE)
     install(FILES ${PROJECT_SOURCE_DIR}/doc/man/${target_name}.1
       DESTINATION ${CMAKE_INSTALL_MANDIR}/man1
+      COMPONENT ${component}
+    )
+  endif()
+  if(INSTALL_ZSH_COMPLETION AND IC_HAS_ZSH_COMPLETION)
+    # Zsh completion files must be prefixed with underscore
+    install(FILES ${PROJECT_SOURCE_DIR}/contrib/completions/zsh/${target_name}.zsh
+      DESTINATION ${CMAKE_INSTALL_DATADIR}/zsh/site-functions
+      RENAME _${target_name}
       COMPONENT ${component}
     )
   endif()
