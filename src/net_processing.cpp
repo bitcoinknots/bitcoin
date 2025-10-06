b7c76e9106af17fd12b82e3264f25d3a77dcb485
Martin Zumsande <mzumsande@gmail.com>
2025-10-06 16:07:19 -0400

    p2p: Add warning message when receiving headers for blocks cached as invalid
    Currently, if database corruption leads to a block being marked as
    invalid incorrectly, we can get stuck in an infinite headerssync
    loop with no indication what went wrong or how to fix it.
    With the added log message, users will receive an explicit warning after each
    failed headerssync attempt with an outbound peer.
    
    Github-Pull: bitcoin/bitcoin#33553
    Rebased-From: 2f51951d03cc1f8917e0fc931dce674f9bfedaf5

--- src/net_processing.cpp
+++ src/net_processing.cpp
@@ -2940,6 +2940,13 @@ void PeerManagerImpl::ProcessHeadersMessage(CNode& pfrom, Peer& peer,
     BlockValidationState state;
     if (!m_chainman.ProcessNewBlockHeaders(headers, /*min_pow_checked=*/true, state, &pindexLast)) {
         if (state.IsInvalid()) {
+            if (!pfrom.IsInboundConn() && state.GetResult() == BlockValidationResult::BLOCK_CACHED_INVALID) {
+                // Warn user if outgoing peers send us headers of blocks that we previously marked as invalid.
+                LogWarning("%s (received from peer=%i). "
+                           "If this happens with all peers, consider database corruption (that -reindex may fix) "
+                           "or a potential consensus incompatibility.",
+                           state.GetDebugMessage(), pfrom.GetId());
+            }
             MaybePunishNodeForBlock(pfrom.GetId(), state, via_compact_block, "invalid header received");
             return;
         }
