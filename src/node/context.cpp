4e3096a62c67d4500288ae42204cef6354ffc26d
Sam <43705650+samsam380@users.noreply.github.com>
2026-04-22 07:19:10 +0700

    Update context.cpp

--- src/node/context.cpp
+++ src/node/context.cpp
@@ -1,7 +1,7 @@
 // Copyright (c) 2019-2022 The Bitcoin Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
-
+#include <stratum/server.h>
 #include <node/context.h>
 
 #include <addrman.h>
