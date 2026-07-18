092f8ea2e406a9bda0f4cc9f5dddf551ae4c25f2
Kyle 🐆 <kyle@privkey.io>
2026-07-18 19:01:39 -0400

    depends: build Qt host tools with the build machine spec

--- depends/packages/qt.mk
+++ depends/packages/qt.mk
@@ -164,14 +164,18 @@ ifneq ($(LTO),)
 $(package)_config_opts_linux += -ltcg
 endif
 
+# -platform is the spec Qt builds qmake and the other host_build tools with, so
+# it has to describe the build machine, which uses build_CXX. Only -xplatform
+# follows host_CXX.
+$(package)_config_opts_linux += -platform linux-g++
 ifneq (,$(findstring clang,$($(package)_cxx)))
   ifneq (,$(findstring -stdlib=libc++,$($(package)_cxx)))
-    $(package)_config_opts_linux += -platform linux-clang-libc++ -xplatform linux-clang-libc++
+    $(package)_config_opts_linux += -xplatform linux-clang-libc++
   else
-    $(package)_config_opts_linux += -platform linux-clang -xplatform linux-clang
+    $(package)_config_opts_linux += -xplatform linux-clang
   endif
 else
-  $(package)_config_opts_linux += -platform linux-g++ -xplatform bitcoin-linux-g++
+  $(package)_config_opts_linux += -xplatform bitcoin-linux-g++
 endif
 
 $(package)_config_opts_mingw32 = -no-opengl
