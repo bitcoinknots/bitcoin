77234285bf670bf58bdcfe3ec6738b4cb2751a2b
Nikola Badjevic <n.badjevic@gmail.com>
2026-09-19 13:47:10 +0200

    changing text from Core to Knots

--- src/clientversion.cpp
+++ src/clientversion.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2012-2022 The Bitcoin Core developers
+// Copyright (c) 2012-2022 The Bitcoin Knots developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -76,7 +76,7 @@ std::string CopyrightHolders(const std::string& strPrefix)
 
     // Make sure Bitcoin Core copyright is not removed by accident
     if (copyright_devs.find("Bitcoin Core") == std::string::npos) {
-        strCopyrightHolders += "\n" + strPrefix + "The Bitcoin Core developers";
+        strCopyrightHolders += "\n" + strPrefix + "The Bitcoin Knots developers";
     }
     return strCopyrightHolders;
 }
